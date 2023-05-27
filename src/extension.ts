0c75ac1556c3c38a75a614a32de7609778b8d67a
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-05-27 23:53:09 +0200

    Fetch abbrs data

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,15 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+import fetch from './lib/fetch';
+
+const origin = 'https://raw.githubusercontent.com/abbrcode/db/main/abbrs/.json';
+
+export async function activate(context: vscode.ExtensionContext) {
+   let data;
+
+   data = await fetch(origin);
+
+   console.log(data);
 }
 
 export function deactivate() { }
