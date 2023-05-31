6013cd929d9e1f7706984ebfec787703e0355f66
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-05-31 18:30:13 +0200

    Event listeners

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { window } from 'vscode';
 
 import abbrs from './abbrs';
 import debounce from './lib/debounce';
@@ -6,8 +7,25 @@ import debounce from './lib/debounce';
 export async function activate(context: ExtensionContext) {
    const abbrVals = await abbrs();
 
+   let activeEditor = window.activeTextEditor;
 
 
+   // Init call
+   decorate();
+
+   // Update when curr active editor changes
+   window.onDidChangeActiveTextEditor(editor => {
+      activeEditor = editor;
+
+      decorate();
+   }, null, context.subscriptions);
+
+   // Update when curr editor text changes
+   workspace.onDidChangeTextDocument(event => {
+      if (!activeEditor) return;
+
+      if (activeEditor.document === event.document) debounce(decorate);
+   }, null, context.subscriptions);
 }
 
 export function deactivate() { }
