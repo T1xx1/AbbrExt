a1927070fc5bfb91ab5419074af6a4600624085f
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-06-02 12:58:04 +0200

    Reusable decoration func

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,20 @@
-import { ExtensionContext, Range, window, workspace } from 'vscode';
+import { ExtensionContext, Range, TextEditorDecorationType, window, workspace } from 'vscode';
 
 import abbrs from './abbrs';
-import { word } from './decorations';
+import { word as wordDecoration } from './decorations';
 
 export async function activate(ctx: ExtensionContext) {
    const abbrVals = await abbrs();
 
    let activeEditor = window.activeTextEditor;
 
-   let decorate = () => {
+   let decorate = (decoration: TextEditorDecorationType, words: string[]) => {
       if (!activeEditor) return;
 
       const document = activeEditor.document;
 
       let rngs = [];
 
-      let words = abbrVals.map(abbr => abbr.word);
       let regex = new RegExp(`\\b\w*${words.join('|')}\w*\\b`, 'gi');
       let matches = [...document.getText().matchAll(regex)];
 
@@ -26,24 +25,30 @@ export async function activate(ctx: ExtensionContext) {
          rngs.push(new Range(startPos, endPos));
       }
 
-      activeEditor.setDecorations(word, rngs);
+      activeEditor.setDecorations(decoration, rngs);
+   };
+
+   let style = () => {
+      let words = abbrVals.map(obj => obj.word);
+
+      decorate(wordDecoration, words);
    };
 
    // Init call
-   decorate();
+   style();
 
    // Update when curr active editor changes
    window.onDidChangeActiveTextEditor(editor => {
       activeEditor = editor;
 
-      decorate();
+      style();
    }, null, ctx.subscriptions);
 
    // Update when curr editor text changes
    workspace.onDidChangeTextDocument(event => {
       if (!activeEditor) return;
 
-      if (activeEditor.document === event.document) decorate();
+      if (activeEditor.document === event.document) style();
    }, null, ctx.subscriptions);
 }
 
