39e1971c3b6fe3b53d0a34669bab2b69865171c5
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-08-06 12:27:21 +0200

    Update decoration colors

--- src/data/decorations.ts
+++ src/data/decorations.ts
@@ -1,9 +1,10 @@
 import { window } from 'vscode';
 
 export const word = window.createTextEditorDecorationType({
-   textDecoration: 'underline wavy #0000ff'
+   textDecoration: 'underline wavy #00f'
 });
 
 export const abbr = window.createTextEditorDecorationType({
-   textDecoration: 'underline dotted #00ff00 3px'
+   textDecoration: 'underline dotted #0f0 3px'
+});
 });
